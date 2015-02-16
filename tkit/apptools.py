040183f391907a7141f8234d4a35f52661a9d8bd
WindfallLabs <garwall101@gmail.com>
2015-02-16 09:17:04 -0700

    Recovered uncompiled apptools.py

---
+++ tkit/apptools.py
@@ -0,0 +1,30 @@
+""" Provides reusable app dev code """
+
+# Imports
+import Tkinter as tk
+import threading
+import logging
+
+
+def build_GUI(App_class):
+    root = tk.Tk()
+    App_class(root).pack(fill='both', expand='yes')
+    root.mainloop()
+
+def thread_GUI(App_class):
+    GUI = ThreadedClient("GUI", lambda: build_GUI(App_class))
+    GUI.start()
+    GUI.join()
+
+class ThreadedClient(threading.Thread):
+    def __init__(self, name, process):
+        """ Subclass of thread allows for easier thread creation """
+        threading.Thread.__init__(self)
+        self.name = name
+        self.process = process
+        
+    def run(self):
+        """ Runs at thread start """
+        logging.debug("{0} thread started".format(self.name))
+        self.process()
+        logging.debug("{0} thread terminated".format(self.name))
