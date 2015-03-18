d5469e9b3856448a8088faebc4b359f5206b0cc1
WindfallLabs <garwall101@gmail.com>
2015-03-18 10:26:48 -0600

    Changed init to import classes as tkit.WidgetClass; testing app moved up a dir

--- tkit/__init__.py
+++ tkit/__init__.py
@@ -1,4 +1,17 @@
-from ._version import get_versions
+#from ._version import get_versions
 
-__version__ = get_versions()['version']
-del get_versions
+#__version__ = get_versions()['version']
+#del get_versions
+
+# Import all classes from modules so that the following psuedo-code
+#  will run.
+
+#import wftkit
+#...
+#self.browse_file = wftkit.BrowseFile(self)
+#self.radiobox = wftkit.Radiobox(self)
+
+from browse_entry import *
+from file_tree import *
+from radiobox import *
+from statusbar import *
