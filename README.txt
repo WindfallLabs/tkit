8c3d0df45e926e58be5c79154a1ab26c6debf9b7
WindfallLabs <garwall101@gmail.com>
2016-09-09 05:05:41 -0600

    Removed tkit.cli -> aside project

---
+++ README.txt
@@ -0,0 +1,2 @@
+Tkit is a GUI creation toolkit that employs Tkinter to make GUI tools for executable
+Python scripts. This project is mostly inactive.
--- tkit/cli/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""tkit.cli: Pleasant Messaging for the Python Shell."""
-
-from tkit.cli._utils import *
-from tkit.cli._nix import *
-from tkit.cli._status import *
